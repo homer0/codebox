ce18113862b68c1786acd012b07e7f78ca82525f
homer0 <me@homer0.dev>
2022-03-17 05:38:43 -0300

    fix: add support for hashed-password in the utility

--- src/utils/get-password.sh
+++ src/utils/get-password.sh
@@ -2,4 +2,17 @@
 
 . ~/.zshrc
 
-echo $(grep -A3 'password:' ~/.config/code-server/config.yaml | head -n1 | awk '{print $2}')
+PASSWORD=$(grep -A3 'password:' ~/.config/code-server/config.yaml | head -n1 | awk '{print $2}')
+PREFIX=""
+# if password is empty
+if [ -z "$PASSWORD" ]; then
+  PASSWORD=$(grep -A3 'hashed-password:' ~/.config/code-server/config.yaml | head -n1 | awk '{print $2}')
+  PREFIX=" (hashed)"
+fi
+
+if [ -z "$PASSWORD" ]; then
+  echo "Password not found in config.yaml"
+  exit 1
+else
+  echo "Password$PREFIX: $PASSWORD"
+fi
