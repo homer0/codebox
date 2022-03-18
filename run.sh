2223c564ec29de695cc3f563e41195a11c4b6362
homer0 <me@homer0.dev>
2022-03-18 05:15:59 -0300

    chore: update run script

--- run.sh
+++ run.sh
@@ -22,17 +22,20 @@ fi
 echo "=== Build image $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
+# CREATE TMP MOUNT PATHS
+echo "=== Create tmp mount paths"
+rm -rf ./.mount
+mkdir -p ./.mount/{config,profile}
+
 # CREATE CONTAINER
 echo "=== Create container $CONTAINER_NAME"
 docker run -d --name $CONTAINER_NAME \
   -p 4522:22 \
   -p 4580:80 \
   -p 6580:8080 \
-  -e GIT_CONFIG_USERNAME="homer0" \
-  -e GIT_CONFIG_EMAIL="me@homer0.dev" \
-  -e CODEBOX_NAME="codebox-test" \
-  -e CODESERVER_PROXY_DOMAIN="homer0.com" \
-  -v $(pwd)/src/ssh:/home/coder/ssh-keys:ro \
+  -v $(pwd)/box-setup:/home/coder/.codebox/setup:ro \
+  -v $(pwd)/.mount/config:/home/coder/.config:rw \
+  -v $(pwd)/.mount/profile:/home/coder/.local/share/code-server:rw \
   -v $(pwd):/home/coder/code:rw \
   $IMAGE_NAME
 
