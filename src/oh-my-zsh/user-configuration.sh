979e66eb95caf4ca157ef8fa8bbe13016ce0c8e3
homer0 <me@homer0.dev>
2022-03-20 03:01:30 -0300

    fix: remove unnecessary aliases

--- src/oh-my-zsh/user-configuration.sh
+++ src/oh-my-zsh/user-configuration.sh
@@ -6,13 +6,8 @@ alias cgst="clear;gst"
 alias cgstb="clear;gst;git branch"
 alias nu="nvm use"
 
-alias httpserver="open http://localhost:8000; python -m SimpleHTTPServer"
-
 gdf(){
   git diff -u $1 | diff-so-fancy | less --tabs=4 -RFS --pattern '^(Date|added|deleted|modified): '
-}
-gzipit(){
-  gzip -c --best $1 > $1.gz
 }
 gpl(){
   git checkout $1; git pull -p; git branch --merged | grep -v \* | grep -v master | grep -v main | xargs -n1 git branch -d
